aa08326cbcbedb15f2fc5c6b70e2231cc82b541a
Ghulam Mustafa <mustafaji@gmail.com>
2022-01-08 22:54:39 +0100

    settings up tailwind CSS

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 text-3xl font-bold underline space-y-4>
+        React - Portfolio Tailwind
+      </h1>
+      <button className='bg-red-500 p-2'>Submitt</button>
+    </div>
+  );
+}
+
+export default App;
