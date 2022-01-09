df81bec3c4ee4600fc3e07c6db648d1d03038203
Ghulam Mustafa <mustafaji@gmail.com>
2022-01-09 22:18:11 +0100

    Added Projects Page

--- src/pages/Courses.js
+++ src/pages/Courses.js
@@ -1,9 +1,9 @@
 import React from "react";
 import Layout from "../compoents/Layout";
-import coursesData from '../resources/courses'
-import AOS from 'aos';
+import coursesData from "../resources/courses";
+import AOS from "aos";
 AOS.init({
-  duration:1000
+  duration: 1000,
 });
 function Courses() {
   return (
@@ -42,23 +42,36 @@ function Courses() {
         <h1 className="text-4xl text-center font-bold mt-5">Because</h1>
       </div>
       <div className="font-bold text-center bg-gray-500 m-20 p-20 text-white rounded-tl-full rounded-br-full md:mx-5">
-        <h1 className="text-8xl md:text-3xl" data-aos='slide-down'>LEARNING IS...</h1>
-        <h1 className="text-8xl md:text-3xl" data-aos='slide-up'>CONTINIUOS</h1>
+        <h1 className="text-8xl md:text-3xl" data-aos="slide-down">
+          LEARNING IS...
+        </h1>
+        <h1 className="text-8xl md:text-3xl" data-aos="slide-up">
+          CONTINIUOS
+        </h1>
       </div>
 
       <div className="grid mt-20 md:grid-cols-1 grid-cols-3 items-center justify-center gap-10 mx-20 md:mx-5">
-            {coursesData.map((cource=>{
-              return <div>
-                  <div className='relative border-2 text-center rounded-tr-3xl rounded-bl-3xl border-gray-400'>
-                     <img src={cource.image}  className='w-full h-60 text-center rounded-tr-3xl rounded-bl-3xl'/>
+        {coursesData.map((cource) => {
+          return (
+            <div>
+              <div className="relative border-2 text-center rounded-tr-3xl rounded-bl-3xl border-gray-400">
+                <img
+                  src={cource.image}
+                  className="w-full h-60 text-center rounded-tr-3xl rounded-bl-3xl"
+                />
 
-                     <div className='absolute inset-0 flex items-center justify-center flex-col opacity-0 bg-black hover:opacity-80 rounded-tr-3xl rounded-bl-3xl'>
-                        <h1 className='text-4xl font-semibold text-white'>{cource.title}</h1>
-                        <button className='border-2 rounded border-white py-2 hover:bg-green-500 px-5 mt-5 text-white'>DEMO</button>
-                     </div>
-                  </div>
+                <div className="absolute inset-0 flex items-center justify-center flex-col opacity-0 bg-black hover:opacity-80 rounded-tr-3xl rounded-bl-3xl">
+                  <h1 className="text-4xl font-semibold text-white">
+                    {cource.title}
+                  </h1>
+                  <button className="border-2 rounded border-white py-2 hover:bg-green-500 px-5 mt-5 text-white">
+                    DEMO
+                  </button>
+                </div>
               </div>
-            }))}
+            </div>
+          );
+        })}
       </div>
     </Layout>
   );
