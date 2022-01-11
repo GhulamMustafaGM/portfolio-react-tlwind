1151b164446e5305a60ee32e5c3e94a0c625cdc5
Ghulam Mustafa <mustafaji@gmail.com>
2022-01-11 23:00:43 +0100

    Added projects

---
+++ src/resources/projects.js
@@ -0,0 +1,46 @@
+const projectsData = [
+  {
+    title: "PIZZASHOP",
+    image: "./ecommerce.svg",
+    description:
+      "An ecommerce application developed using react , node , redux and mongodb",
+    link: "/",
+  },
+  {
+    title: "HITPIZZA",
+    image: "./pizzas.svg",
+    description:
+      "An ecommerce application developed using react , node , redux and mongodb",
+    link: "/",
+  },
+  {
+    title: "SHOWROOMS",
+    image: "./hotels.svg",
+    description:
+      "An ecommerce application developed using react , node , redux and mongodb",
+    link: "/",
+  },
+  {
+    title: "JOBSAPPLY",
+    image: "./jobs.svg",
+    description:
+      "An ecommerce application developed using react , node , redux and mongodb",
+    link: "/",
+  },
+  {
+    title: "ADAMGRAM",
+    image: "./socialmedia.svg",
+    description:
+      "An ecommerce application developed using react , node , redux and mongodb",
+    link: "/",
+  },
+  {
+    title: "ECARS",
+    image: "./cars.svg",
+    description:
+      "An ecommerce application developed using react , node , redux and mongodb",
+    link: "/",
+  },
+];
+
+export default projectsData;
